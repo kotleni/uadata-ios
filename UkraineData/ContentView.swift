2236a1bb04181a3bfd5ee78f233807695c6028d1
kotleni <kotleni@icloud.com>
2022-06-16 22:34:30 +0300

    Wiki url fix

--- UkraineData/ContentView.swift
+++ UkraineData/ContentView.swift
@@ -61,7 +61,7 @@ struct AboutView: View {
                     .foregroundColor(Color("LinkColor"))
                     
                     Button {
-                        openLink(url: "https://uk.wikipedia.org/wiki/Російське_вторгнення_в_Україну_(2022)")
+                        openLink(url: "https://uk.wikipedia.org/wiki/%D0%A0%D0%BE%D1%81%D1%96%D0%B9%D1%81%D1%8C%D0%BA%D0%B5_%D0%B2%D1%82%D0%BE%D1%80%D0%B3%D0%BD%D0%B5%D0%BD%D0%BD%D1%8F_%D0%B2_%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D1%83_%282022%28")
                     } label: {
                         Text("link_wiki".localized)
                     }
