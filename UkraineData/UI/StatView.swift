153f35f98ead148b0fc71d73173f24531bbee897
kotleni <kotleni@icloud.com>
2022-06-17 12:12:38 +0300

    Project views moved to UI directory

--- UkraineData/UI/StatView.swift
+++ UkraineData/UI/StatView.swift
@@ -1,93 +1,11 @@
+//
+//  StatView.swift
+//  UkraineData
+//
+//  Created by Victor Varenik on 17.06.2022.
+//
+
 import SwiftUI
-
-struct ContentView: View {
-    @State var selection: Int = 0
-    
-    var body: some View {
-        TabView(selection: $selection) {
-            StatView()
-                .tag(0)
-                .tabItem {
-                    Label("tab_stat".localized, systemImage: "doc.plaintext")
-                }
-            MapView()
-                .tag(1)
-                .tabItem {
-                    Label("tab_map".localized, systemImage: "map")
-                        .contextMenu {
-                            
-                        }
-                }
-            AboutView()
-                .tag(2)
-                .tabItem {
-                    Label("tab_about".localized, systemImage: "info.circle")
-                }
-            
-        }
-        .navigationTitle(Text(selection == 0 ? "title_stat".localized : selection == 1 ? "title_map".localized : "title_about".localized))
-    }
-    
-}
-
-struct MapView: View {
-    var body: some View {
-        WebView(url: "https://upload.wikimedia.org/wikipedia/commons/4/4f/2022_Russian_invasion_of_Ukraine.svg")
-    }
-    
-    private func openLink(url: String) {
-        guard let url = URL(string: url) else { return }
-        UIApplication.shared.open(url)
-    }
-}
-
-struct AboutView: View {
-    var body: some View {
-        VStack {
-            Form {
-                Section {
-                    Button {
-                        openLink(url: "https://github.com/kotleni/uadata-ios")
-                    } label: {
-                        Text("link_github".localized)
-                    }
-                    .foregroundColor(Color("LinkColor"))
-
-                    Button {
-                        openLink(url: "https://uadata.net/api")
-                    } label: {
-                        Text("link_uadata".localized)
-                    }
-                    .foregroundColor(Color("LinkColor"))
-                    
-                    Button {
-                        openLink(url: "https://uk.wikipedia.org/wiki/%D0%A0%D0%BE%D1%81%D1%96%D0%B9%D1%81%D1%8C%D0%BA%D0%B5_%D0%B2%D1%82%D0%BE%D1%80%D0%B3%D0%BD%D0%B5%D0%BD%D0%BD%D1%8F_%D0%B2_%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D1%83_%282022%28")
-                    } label: {
-                        Text("link_wiki".localized)
-                    }
-                    .foregroundColor(Color("LinkColor"))
-
-                } header: {
-                    Text("header_links")
-                }
-                
-                Section {
-                    Text("about_version")
-                        .foregroundColor(Color("TipColor"))
-                    Text("about_developer")
-                        .foregroundColor(Color("TipColor"))
-                } header: {
-                    Text("header_desc")
-                }
-            }
-        }
-    }
-    
-    private func openLink(url: String) {
-        guard let url = URL(string: url) else { return }
-        UIApplication.shared.open(url)
-    }
-}
 
 struct StatView: View {
     private let groundItems = [
@@ -219,3 +137,4 @@ struct StatView: View {
         }
     }
 }
+
